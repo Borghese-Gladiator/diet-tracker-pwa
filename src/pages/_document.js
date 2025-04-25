4e4baf1d1b849c42f0567dd42a4fbad5dbb378a7
timothysheee <157530836+timothysheee@users.noreply.github.com>
2025-04-25 17:34:12 -0400

    feat: _document.js - update meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* DEFAULT Meta */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Diet Tracker" />
-        <meta name="description" content="Track your diet and nutrition goals" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
+        {/* CUSTOM Meta */}
         <meta name="application-name" content="Diet Tracker" />
+        <meta name="description" content="No-fuss diet tracking with big buttons, big font, and a big graph. " />
       </Head>
       <body>
         <Main />
