0c74c65a418a31b5db6b86e70fdacf565339e22b
timothysheee <157530836+timothysheee@users.noreply.github.com>
2025-04-25 16:59:28 -0400

    fix: Track Food - increase NumberInput font size

--- src/pages/index.js
+++ src/pages/index.js
@@ -186,7 +186,7 @@ export default function Home() {
                                 }))
                               }
                               precision={2}
-                              size="sm"
+                              size="lg"
                               w="80px"
                             >
                               <NumberInputField textAlign="center" />
